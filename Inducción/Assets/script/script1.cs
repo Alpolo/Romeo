84f07335e22efb980d5e1fc5c72e9df93c9bc38d
Alpolo <edisonberrio048@gmail.com>
2017-04-16 12:58:40 -0500

    Record 14 Abril-17
    Primera Clase

---
+++ Inducción/Assets/script/script1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour 
+{
+	void Awake()
+	{
+		print ("Hola Awake");
+	}
+
+
+	void Start () 
+	{
+		Debug.Log ("Hola Stat");
+	}
+	
+
+	void Update ()
+	{
+		print ("Hola Update");
+	}
+
+	void FixedUpdate ()
+	{
+		Debug.Log ("Hola FixedUpdate");
+	}
+}
